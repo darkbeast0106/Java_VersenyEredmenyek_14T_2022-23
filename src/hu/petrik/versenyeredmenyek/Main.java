52cf84c410eb4b88eb0f44cdee063fac9342a125
Budaházi Bence <budahazi.bence@petrik.hu>
2022-10-20 10:54:20 +0200

    Forrásfájl beolvasása

--- src/hu/petrik/versenyeredmenyek/Main.java
+++ src/hu/petrik/versenyeredmenyek/Main.java
@@ -1,8 +1,31 @@
 package hu.petrik.versenyeredmenyek;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
-
+    private static Map<String, List<Eredmeny>> sportagEredmenyek = new HashMap<>();
     public static void main(String[] args) {
+        String fajlNev = "eredmenyek.txt";
+        try {
+            beolvas(fajlNev);
+        } catch (FileNotFoundException e) {
+            System.out.printf("Nem található a %s fájl", fajlNev);
+        }
+    }
 
+    private static void beolvas(String fajlNev) throws FileNotFoundException {
+        Scanner file = new Scanner(new File(fajlNev));
+        while (file.hasNext()){
+            String[] sor = file.nextLine().split(" ");
+            String sportag = sor[0];
+            String reszIdo = sor[1];
+            String nev = sor[2] + " " + sor[3];
+            sportagEredmenyek.putIfAbsent(sportag, new ArrayList<>());
+            Eredmeny eredmeny = new Eredmeny(reszIdo, nev);
+            sportagEredmenyek.get(sportag).add(eredmeny);
+        }
+        file.close();
     }
 }
